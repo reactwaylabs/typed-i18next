7c7f52544ab60cdc79af1c19b4ac5930c7c4c8bf
Simranjyot Singh <simranjyot.gill@thoughtspot.com>
2020-02-04 15:56:09 -0800

    add support for string interpolation and add support to use keys without namespace in translate function

--- react.d.ts
+++ react.d.ts
@@ -6,18 +6,18 @@ export interface StrictTypedTranslations<
     TTranslations extends { keys: { [key: string]: any }; keysWithNS: { [key: string]: any } }
 > {
     (ns?: undefined, ...args: IgnoreFirstParameter<TTranslationsFun>): Omit<ReturnType<TTranslationsFun>, "t"> & {
-        t: (key: ExtractObject<TTranslations["keysWithNS"]>) => string;
+        t: (key: ExtractObject<TTranslations["keysWithNS"]> | ExtractObject<TTranslations["keys"]>, interpolate?: Object) => string;
     };
     <TNamespace extends keyof TTranslations["keys"]>(ns: TNamespace, ...args: IgnoreFirstParameter<TTranslationsFun>): Omit<
         ReturnType<TTranslationsFun>,
         "t"
     > & {
-        t: (key: TTranslations["keys"][TNamespace]) => string;
+        t: (key: TTranslations["keys"][TNamespace], interpolate?: Object) => string;
     };
     <TNamespaces extends ArrayLike<keyof TTranslations["keys"]>>(ns: TNamespaces, ...args: IgnoreFirstParameter<TTranslationsFun>): Omit<
         ReturnType<TTranslationsFun>,
         "t"
     > & {
-        t: (key: ExtractObject<Pick<TTranslations["keysWithNS"], TNamespaces[number]>>) => string;
+        t: (key: ExtractObject<Pick<TTranslations["keysWithNS"], TNamespaces[number]>>, interpolate?: Object) => string;
     };
 }
