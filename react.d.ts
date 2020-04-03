c7bfd32841207d2e2730728cd17f2fe7f4cd0e9a
Eimantas Zubavičius <eimantas.zubavicius@quatrodev.com>
2020-04-03 15:30:42 +0300

    Hotfix: Add translation variables. (#13)
    * Add translation variables.
    
    * Bump version.
    
    * Trigger CI.

--- react.d.ts
+++ react.d.ts
@@ -6,18 +6,18 @@ export interface StrictTypedTranslations<
     TTranslations extends { keys: { [key: string]: any }; keysWithNS: { [key: string]: any } }
 > {
     (ns?: undefined, ...args: IgnoreFirstParameter<TTranslationsFun>): Omit<ReturnType<TTranslationsFun>, "t"> & {
-        t: (key: ExtractObject<TTranslations["keysWithNS"]>) => string;
+        t: (key: ExtractObject<TTranslations["keysWithNS"]>, variables?: {[key: string]: unknown}) => string;
     };
     <TNamespace extends keyof TTranslations["keys"]>(ns: TNamespace, ...args: IgnoreFirstParameter<TTranslationsFun>): Omit<
         ReturnType<TTranslationsFun>,
         "t"
     > & {
-        t: (key: TTranslations["keys"][TNamespace]) => string;
+        t: (key: TTranslations["keys"][TNamespace], variables?: {[key: string]: unknown}) => string;
     };
     <TNamespaces extends ArrayLike<keyof TTranslations["keys"]>>(ns: TNamespaces, ...args: IgnoreFirstParameter<TTranslationsFun>): Omit<
         ReturnType<TTranslationsFun>,
         "t"
     > & {
-        t: (key: ExtractObject<Pick<TTranslations["keysWithNS"], TNamespaces[number]>>) => string;
+        t: (key: ExtractObject<Pick<TTranslations["keysWithNS"], TNamespaces[number]>>, variables?: {[key: string]: unknown}) => string;
     };
 }
